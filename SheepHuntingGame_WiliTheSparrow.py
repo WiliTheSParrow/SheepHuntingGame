ee40c3288c8a65b423e831389fad896e9cffef1b
WiliTheSparrow <w.julia91@gmail.com>
2019-01-18 15:48:54 +0100

    2nd commit

--- SheepHuntingGame_WiliTheSparrow.py
+++ SheepHuntingGame_WiliTheSparrow.py
@@ -27,7 +27,6 @@ import random
 van_birka = 'o'
 nincs_birka = ' '
 serult_birka = 's'
-halott_birka = 'x'
 
 
 def jatekos_udvozlese():
@@ -43,15 +42,129 @@ def jatekos_udvozlese():
     print('Minden o jelzés egy birkát jelöl, aki szeretné, ha békén hagynád, de te nem fogod.')
     print('A sor (1,2,3...) és oszlop (A,B,C...) koordináták segítségével tudod leadni a lövéseidet.')
     input(' ')
-    print('Ne bízd el magad! A leadott lövéseidet meghallják és odébb ugranak. Ha valamelyikük eléri a karám szélét\nvesztettél!')
+    print(
+        'Ne bízd el magad! A leadott lövéseidet meghallják és odébb ugranak. Ha valamelyikük eléri a karám szélét\nvesztettél!')
     input(' ')
-    print('Nem biztos, hogy mindig jól célzol... Előfordulhatnak sérült birkák is (S), akik ugyanúgy küzenek az életükért.')
+    print(
+        'Nem biztos, hogy mindig jól célzol... Előfordulhatnak sérült birkák is (S), akik ugyanúgy küzenek az életükért.')
     input(' ')
     print('Lássuk mire vagy képes...')
 
 
-
 def tabla_megjelenites(birka):
+    # <editor-fold desc="Koordinatak es birkak">
+    a1 = birka[0]
+    b1 = birka[1]
+    c1 = birka[2]
+    d1 = birka[3]
+    e1 = birka[4]
+    f1 = birka[5]
+    g1 = birka[6]
+    h1 = birka[7]
+    i1 = birka[8]
+    j1 = birka[9]
+    k1 = birka[10]
+    a2 = birka[11]
+    b2 = birka[12]
+    c2 = birka[13]
+    d2 = birka[14]
+    e2 = birka[15]
+    f2 = birka[16]
+    g2 = birka[17]
+    h2 = birka[18]
+    i2 = birka[19]
+    j2 = birka[20]
+    k2 = birka[21]
+    a3 = birka[22]
+    b3 = birka[23]
+    c3 = birka[24]
+    d3 = birka[25]
+    e3 = birka[26]
+    f3 = birka[27]
+    g3 = birka[28]
+    h3 = birka[29]
+    i3 = birka[30]
+    j3 = birka[31]
+    k3 = birka[32]
+    a4 = birka[33]
+    b4 = birka[34]
+    c4 = birka[35]
+    d4 = birka[36]
+    e4 = birka[37]
+    f4 = birka[38]
+    g4 = birka[39]
+    h4 = birka[40]
+    i4 = birka[41]
+    j4 = birka[42]
+    k4 = birka[43]
+    a5 = birka[44]
+    b5 = birka[45]
+    c5 = birka[46]
+    d5 = birka[47]
+    e5 = birka[48]
+    f5 = birka[49]
+    g5 = birka[50]
+    h5 = birka[51]
+    i5 = birka[52]
+    j5 = birka[53]
+    k5 = birka[54]
+    a6 = birka[55]
+    b6 = birka[56]
+    c6 = birka[57]
+    d6 = birka[58]
+    e6 = birka[59]
+    f6 = birka[60]
+    g6 = birka[61]
+    h6 = birka[62]
+    i6 = birka[63]
+    j6 = birka[64]
+    k6 = birka[65]
+    a7 = birka[66]
+    b7 = birka[67]
+    c7 = birka[68]
+    d7 = birka[69]
+    e7 = birka[70]
+    f7 = birka[71]
+    g7 = birka[72]
+    h7 = birka[73]
+    i7 = birka[74]
+    j7 = birka[75]
+    k7 = birka[76]
+    a8 = birka[77]
+    b8 = birka[78]
+    c8 = birka[79]
+    d8 = birka[80]
+    e8 = birka[81]
+    f8 = birka[82]
+    g8 = birka[83]
+    h8 = birka[84]
+    i8 = birka[85]
+    j8 = birka[86]
+    k8 = birka[87]
+    a9 = birka[88]
+    b9 = birka[89]
+    c9 = birka[90]
+    d9 = birka[91]
+    e9 = birka[92]
+    f9 = birka[93]
+    g9 = birka[94]
+    h9 = birka[95]
+    i9 = birka[96]
+    j9 = birka[97]
+    k9 = birka[98]
+    a10 = birka[99]
+    b10 = birka[100]
+    c10 = birka[101]
+    d10 = birka[102]
+    e10 = birka[103]
+    f10 = birka[104]
+    g10 = birka[105]
+    h10 = birka[106]
+    i10 = birka[107]
+    j10 = birka[108]
+    k10 = birka[109]
+    # </editor-fold>
+
     os.system('cls||clear')
     print('')
     print('')
@@ -90,7 +203,6 @@ def tabla_megjelenites(birka):
     print('')
 
 
-
 def birkak_kipakolasa():
     random_birka_kirakas = []
     birka_vane = ['o', ' ']
@@ -109,7 +221,15 @@ def birkak_kipakolasa():
             random_birka_kirakas.append(i)
     return random_birka_kirakas
 
+
 jatekos_udvozlese()
 tabla_megjelenites(birkak_kipakolasa())
 
 jatekos_koordinatak_megadasa = input('Add meg a lövés koordinátáid (sor/oszlop): ').lower()
+
+if jatekos_koordinatak_megadasa == 'o':
+    halott_birka = print("Birka találat!")
+    serult_birka = print("Eltaláltad, de nem volt pontos a célzás. Sérült birka.")
+    random.randint(halott_birka, serult_birka)
+else:
+    print("Nem talált!")
